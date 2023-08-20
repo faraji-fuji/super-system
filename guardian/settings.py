51647e290942008677c74496960eb4cf26d21cd1
Faraji Ombonya <farajiombonya@gmail.com>
2023-08-20 15:32:52 +0300

    sms settings

--- guardian/settings.py
+++ guardian/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ip4&q8%+65r0#&uv%p(_qhvdya$#d59d@a55*ji55&3g+my02'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMS
+SMS_GATEWAY_API_KEY = config('SMS_GATEWAY_API_KEY')
+SMS_SENDER_ID = config('SMS_SENDER_ID')
