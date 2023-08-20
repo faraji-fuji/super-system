96cfde4a455d5a24250a2a01cc35c3b895243cb3
Faraji Ombonya <farajiombonya@gmail.com>
2023-08-20 15:26:26 +0300

    get user serializer, verify otp serializer, resend otp serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -6,3 +6,30 @@ class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'phone_number', 'password']
+
+
+class GetUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'email', 
+            'first_name', 
+            'last_name', 
+            'phone_number', 
+            'otp', 
+            'otp_expiry', 
+            'last_login', 
+            'is_active',
+            'is_phone_verified', 
+            'is_email_verified', 
+            'is_verified',
+            ]
+
+
+class VerifyOtpSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    otp = serializers.IntegerField()
+
+
+class ResendOtpSerializer(serializers.Serializer):
+    email = serializers.EmailField()
